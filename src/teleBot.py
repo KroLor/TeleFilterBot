87f5d18798560f75dcf37211e0a7491a75e8038f
FeelYon <tokpli33@mail.ru>
2025-05-31 00:23:55 +0400

    v0.1

--- src/teleBot.py
+++ src/teleBot.py
@@ -5,47 +5,48 @@ import telebot
 def paused():
     input("Запуск приостановлен. Нажмите Enter, если исправили это.")
 
-def checkStatusBot():
-    log_and_print("info", "Инициализация...")
+# def checkStatusBot():
+#     log_and_print("info", "Инициализация...")
 
-    updates = bot.get_updates()
+#     updates = bot.get_updates()
 
-    while (True):
-        if not updates:
-            log_and_print("error", "Бот находится не в группе, убедитесь, что он добавлен в неё.")
-            return 1
-        elif updates[-1].message:
-            last_update = updates[-1]
-            break
-        else:
-            log_and_print("error", "Сообщений не обнаружено, бот не знает в какой он группе.")
-            paused()
-            updates = bot.get_updates()
-            continue
+#     while (True):
+#         if not updates:
+#             log_and_print("error", "Бот находится не в группе, убедитесь, что он добавлен в неё.")
+#             return 1
+#         elif updates[-1].message:
+#             last_update = updates[-1]
+#             break
+#         else:
+#             log_and_print("error", "Сообщений не обнаружено, бот не знает в какой он группе.")
+#             paused()
+#             updates = bot.get_updates()
+#             continue
     
-    chat = bot.get_chat(last_update.message.chat.id)
+#     chat = bot.get_chat(last_update.message.chat.id)
 
-    member = bot.get_chat_member(chat.id, bot.get_me().id)
-    if member.status != "administrator" or member.can_delete_messages == False:
-        log_and_print("warning", "Бот не является администратором или ему запрещено удалять сообщения, " + 
-                      "возможности бота ограничены.")
-        log_and_print("debug", f"Статус: status: {member.status}, can_delete_messages: {member.can_delete_messages}")
-        return -1
-    else:
-        log_and_print("info", "Успешно.")
-        chat_name = chat.title if chat.title else chat.username
-        log_and_print("debug", f"Бот в группе: {chat_name}, id: {chat.id}")
-        return 0
+#     member = bot.get_chat_member(chat.id, bot.get_me().id)
+#     if member.status != "administrator" or member.can_delete_messages == False:
+#         log_and_print("warning", "Бот не является администратором или ему запрещено удалять сообщения, " + 
+#                       "возможности бота ограничены.")
+#         log_and_print("debug", f"Статус: status: {member.status}, can_delete_messages: {member.can_delete_messages}")
+#         return -1
+#     else:
+#         log_and_print("info", "Успешно.")
+#         chat_name = chat.title if chat.title else chat.username
+#         log_and_print("debug", f"Бот в группе: {chat_name}, id: {chat.id}")
+#         return 0
 
 def startBot(API_TOKEN):
     global bot
 
     bot = telebot.TeleBot(API_TOKEN)
+    bot.delete_webhook()
 
-    status = checkStatusBot()
-    while (status != 0):
-        paused()
-        status = checkStatusBot()
+    # status = checkStatusBot()
+    # while (status != 0):
+    #     paused()
+    #     status = checkStatusBot()
     
     @bot.message_handler(func=lambda message: True)
     def handler_message(message):
@@ -53,13 +54,13 @@ def startBot(API_TOKEN):
             message_text = message.text
             prohibited = searching(message_text)
             if prohibited == True:
-                log_and_print("info", f"Обнаружен запрещенный контент от {message.from_user.username} (@{message.from_user.username}):\n" 
-                              + message_text)
+                log_and_print("info", f"Обнаружен запрещенный контент в чате {message.chat.title} от @{message.from_user.username}.")
                 try:
                     bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
-                    log_and_print("info", f"Сообщение {message.from_user.username} (@{message.from_user.username}) удалено.")
+                    log_and_print("info", f"Сообщение @{message.from_user.username} удалено.")
                 except Exception as e:
-                    log_and_print("error", f"Не удалось удалить сообщение {message.from_user.username}: {e}")
+                    log_and_print("error", f"Не удалось удалить сообщение @{message.from_user.username} в чате {message.chat.title}: {e}.")
+                    log_and_print("debug", f"Не удалось удалить сообщение @{message.from_user.username} из чата {message.chat.title} ({message.chat}):\n{message_text}")
 
     log_and_print("info", "Бот запущен.")
     bot.polling(none_stop=True, interval=0, timeout=20)
